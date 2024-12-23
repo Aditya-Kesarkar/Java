153af5f1ab53df15bd430a09b8f2558970b774b3
Aditya-Kesarkar <adityakesarkar29@gmail.com>
2024-12-23 19:21:10 +0530

    Program for printing an arrow pattern using "*"

---
+++ ArrowPattern.java
@@ -0,0 +1,24 @@
+public class ArrowPattern {
+ public static void main(String[] args) {
+    System.out.println("Try programiz.pro");
+    for (int i= 1; i<6;i++) {
+        for (int j = 5; j > i;j--) {
+            System.out.print(" ");
+        }
+        for (int k = 1; k <6; k++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    for (int i = 1; i<5; i++) {
+        for (int j = 1; j<=i;j++) {
+            System.out.print(" ");
+        }
+        for (int k = 1; k < 6;k++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+    }
