890a5eec460606627cd1f3a16fd6ee70118ee8b1
Aditya-Kesarkar <adityakesarkar29@gmail.com>
2024-12-23 19:28:52 +0530

    Program for printing the character type i.e vowel, consonant, number, special character

---
+++ CharacterType.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class CharacterType {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the character: ");
+        char ch = sc.next().charAt(0);
+        
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            System.out.println(ch + " is a vowel.");
+        } 
+        else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            System.out.println(ch + " is a consonant.");
+        } 
+        else if (Character.isDigit(ch)) {
+            System.out.println(ch + " is a number.");
+        } 
+        else {
+            System.out.println(ch + " is a special character.");
+        }
+    }
+}
