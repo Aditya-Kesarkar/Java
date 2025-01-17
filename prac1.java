88701fbdb8a0f08603d3e1a223bb76b63e190c86
Aditya-Kesarkar <adityakesarkar29@gmail.com>
2025-01-17 20:06:24 +0530

    simple java program to print har-coded values on console

---
+++ prac1.java
@@ -0,0 +1,10 @@
+class prac1 {
+	public static void main (String[] args){
+		var age = 22;
+		var name = "Aditya";
+		var percentage = 70;
+		System.out.println("Age is : "+ age);
+		System.out.println("Name is : "+ name);
+		System.out.println("Percentage is : "+ percentage);
+	}
+}
