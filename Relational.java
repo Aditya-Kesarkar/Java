5b3ec9726262f96c42f0fc7206786927d0961707
Aditya Kesarkar <adityakesarkar29@gmail.com>
2025-02-18 15:12:31 +0530

    Relational operators in Java

---
+++ Relational.java
@@ -0,0 +1,20 @@
+class Relational
+{
+	public static void main(String[] args) 
+	{
+		var a = 10;
+		var b = 5;
+		var c = a<b;
+		var d = a>b;
+		var e = a>=b;
+		var f = a<=b;
+		var g = a==b;
+		var h = a!=b;
+		System.out.println("c is " + c);
+		System.out.println("d is " + d);
+		System.out.println("e is " + e);
+		System.out.println("f is " + f);
+		System.out.println("g is " + g);
+		System.out.println("h is " + h);
+	}	
+}
